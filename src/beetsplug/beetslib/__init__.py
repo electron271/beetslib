baf6329ae85eb593605a97a75267a58892884417
electron271 <66094410+electron271@users.noreply.github.com>
2026-04-17 02:42:28 -0500

    im just stupid

--- src/beetsplug/beetslib/__init__.py
+++ src/beetsplug/beetslib/__init__.py
@@ -5,7 +5,7 @@ from beets import ui
 from beets.library import Album, Library
 from beets.plugins import BeetsPlugin
 from beets.ui import Subcommand
-from multiprocessing.pool import ThreadPool
+from multiprocessing.pool import AsyncResult, ThreadPool
 
 
 class BeetsLib(BeetsPlugin):
@@ -21,8 +21,7 @@ class BeetsLib(BeetsPlugin):
         ):  # im just doing this instead of exists() to raise an error if its a file
             self.opusdir.mkdir(parents=False, exist_ok=True)
 
-    def _flac_to_opus(self, files: tuple[Path, Path]):
-        flac_file, opus_file = files
+    def _flac_to_opus(self, flac_file: Path, opus_file: Path):
         self._log.info(f"converting {flac_file} to {opus_file}")
         subprocess.run(
             [
@@ -34,50 +33,17 @@ class BeetsLib(BeetsPlugin):
                 flac_file,
                 opus_file,
             ],
-            capture_output=False,
+            capture_output=True,
         )
         self._log.info(f"done converting {flac_file} to {opus_file}")
 
     def _replaygain_album(self, files):
-        self._log.debug(f"calculating replaygain for files: {files}")
+        self._log.info(f"calculating replaygain for files: {files}")
         subprocess.run(
             ["rsgain", "custom", "--album", "--tagmode=i", "--opus-mode=s", *files],
             capture_output=True,
         )
-        self._log.debug(f"done calculating replaygain for files: {files}")
-
-    def _process_album(self, album: Album):
-        self._log.info(f"processing album: {album.album}")
-        tracks = album.items()
-
-        self._log.info(f"calculating replaygain for album: {album.album}")
-        replaygain = self.pool.apply_async(
-            self._replaygain_album, ([str(track.filepath) for track in tracks],)
-        )
-
-        starmap = []
-        for track in tracks:
-            if track.format != "FLAC":  # TODO: add better handling for this probably
-                raise ValueError(f"track {track.filepath} isnt a flac")
-
-            self._log.debug(f"processing track: {track.filepath}")
-            starmap.append(
-                (
-                    track.filepath,
-                    Path(
-                        track.destination(basedir=self.opusdir.__bytes__()).decode()
-                    ).with_suffix(".opus"),
-                )
-            )
-        self.pool.map(self._flac_to_opus, starmap)
-
-        self._log.info(f"calculating replaygain for converted album: {album.album}")
-        self._replaygain_album([str(opus_file) for _, opus_file in starmap])
-
-        if not replaygain.ready():
-            replaygain.wait()
-
-        self._log.info(f"done processing album: {album.album}")
+        self._log.info(f"done calculating replaygain for files: {files}")
 
     def commands(self):
         reconvert = Subcommand(
@@ -96,6 +62,39 @@ class BeetsLib(BeetsPlugin):
         albums = lib.albums()
 
         ui.print_("converting albums...")
-        self.pool.map(self._process_album, albums)
+        results = []
+        needs_replaygain_results: list[tuple[Album, AsyncResult]] = []
+        for album in albums:
+            tracks = album.items()
+
+            results.append(
+                self.pool.apply_async(
+                    self._replaygain_album, ([str(track.filepath) for track in tracks],)
+                )
+            )
+
+            starmap = []
+            for track in tracks:
+                if (
+                    track.format != "FLAC"
+                ):  # TODO: add better handling for this probably
+                    raise ValueError(f"track {track.filepath} isnt a flac")
+
+                self._log.debug(f"processing track: {track.filepath}")
+                starmap.append(
+                    (
+                        track.filepath,
+                        Path(
+                            track.destination(basedir=self.opusdir.__bytes__()).decode()
+                        ).with_suffix(".opus"),
+                    )
+                )
+            needs_replaygain_results.append(
+                (album, self.pool.starmap_async(self._flac_to_opus, starmap))
+            )
+
+        for album, result in needs_replaygain_results:
+            result.wait()
+            self._replaygain_album([str(opus_file) for _, opus_file in starmap])
 
         ui.print_("done")
